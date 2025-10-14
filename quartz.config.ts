a5ff19d95067205295bf56cd049189afdea45303
Benni <benni@bennis-mbp.speedport.ip>
2025-10-14 21:20:51 +0200

    Quartz sync: Oct 14, 2025, 9:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "OKKULTRA",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Boldonse",
+        body: "JetBrains Mono",
         code: "IBM Plex Mono",
       },
       colors: {
