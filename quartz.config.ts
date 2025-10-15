8a225e0b9522d29269d613fbf2cef4b26e2b2770
ncrnmcn <benjamin.schaefer84@gmail.com>
2025-10-15 09:25:27 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Special Elite",
+        header: "Atkinson Hyperlegible Next",
         body: "Atkinson Hyperlegible Next",
         code: "IBM Plex Mono",
       },
