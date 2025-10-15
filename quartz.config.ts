33435702c451f2ba458ed0c75720214f9c25a00f
ncrnmcn <benjamin.schaefer84@gmail.com>
2025-10-15 13:21:54 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Staatliches",
+        header: "Mozilla Headline",
         body: "Atkinson Hyperlegible Next",
         code: "IBM Plex Mono",
       },
