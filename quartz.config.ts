ffd8aec56f36ea041546b35f654e960209dc8be4
ncrnmcn <benjamin.schaefer84@gmail.com>
2025-10-15 15:52:46 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://ncrnmcn.github.io/okkultra/",
+    baseUrl: "https://ncrnmcn.github.io/okkultra",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
