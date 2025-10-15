f6eb1e09ee713d323471a801151bf6162cef6811
ncrnmcn <benjamin.schaefer84@gmail.com>
2025-10-15 14:22:56 +0200

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "OKKULTRA",
+    pageTitle: "Ωkkultra",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Overpass Mono",
+        header: "Advent Pro",
         body: "Atkinson Hyperlegible Next",
         code: "IBM Plex Mono",
       },
